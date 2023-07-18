c5fcfdb1e405388d7f550197cbe3b165ff3de459
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-18 16:35:37 -0300

    Carregando background

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ y = 720
 screen = pygame.display.set_mode((x,y))
 pygame.display.set_caption("Ship Attacks")
 
+bg = pygame.image.load('images/bg.jpg').convert_alpha()
+bg = pygame.transform.scale(bg, (x,y))
+
 rodando = True
 
 while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
-pygame.display.update()
+    
+    screen.blit(bg,(0,0))
+    pygame.display.update()
