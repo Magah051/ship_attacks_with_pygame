5d0e4b58d190c80eef802d87ca181eeb3f5302bd
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-18 16:32:09 -0300

    Difinições de inicialização de tela

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pygame;
 
 
 pygame.init
+
+x = 1280
+y = 720
+
+screen = pygame.display.set_mode((x,y))
+pygame.display.set_caption("Ship Attacks")
+
+rodando = True
+
+while rodando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+pygame.display.update()
