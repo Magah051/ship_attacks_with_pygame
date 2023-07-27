5398ee9ae2037d7a04f7b93397455c3f503c9d88
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-27 00:01:17 -0300

    Movimentação do player com as teclas

--- main.py
+++ main.py
@@ -39,10 +39,16 @@ while rodando:
     screen.blit(bg, (rel_x - bg.get_rect().width, 0))
     if rel_x < 1280:
         screen.blit(bg, (rel_x, 0))
+
+    tecla = pygame.key.get_pressed()
+    if (tecla[pygame.K_UP] or tecla[pygame.K_w]) and pos_player_y > 1:
+        pos_player_y -=1
+    if (tecla[pygame.K_DOWN] or tecla[pygame.K_s]) and pos_player_y < 665:
+        pos_player_y +=1    
     
-    x-=1
+    x-=2
 
     screen.blit(alien, (pos_alien_x, pos_alien_y))
-    screen.blit(playerImg, (pos_player_x, pos_alien_y))
+    screen.blit(playerImg, (pos_player_x, pos_player_y))
 
     pygame.display.update()
